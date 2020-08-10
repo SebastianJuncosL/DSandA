1558ca7be344aebb5557e6a7e2dc978e374a2121
Sebastian Juncos <sebastianjuncos@gmail.com>
2020-08-10 12:58:35 -0500

    finshed excersise  and 2

--- Calentamiento.cpp
+++ Calentamiento.cpp
@@ -49,3 +49,81 @@ void suma()
 }
 
 // 3-------------------------------------------------------------------------
+
+// void palabraRepetida()
+// {
+//     string palabra;
+//     string oracion;
+//     int count = 0;
+//     int j=0;
+//     char dummy;
+//     std::cout << "Introduce una oración:\t";
+//     getline(cin, oracion);
+//     std::cout << "Introduce una palabra:\t";
+//     cin>>palabra;
+//     int palabraLength = palabra.length();
+//     // for (char const &c:oracion)
+//     // {
+//     //     if (c == palabra[i])
+//     //     {
+//     //         count++;
+//     //         i++;
+//     //     }
+//     //     else if(c!= palabra[i])
+//     //         i=0;
+//     //     if (i<= palabraLength)
+//     //     {
+            
+//     //         cout<<c<<" "<<i<<" "<<count<<"\n";
+//     //         i=0;
+//     //     } 
+//     // }
+
+//     // for(char const &c:oracion)
+//     // {
+//     //     dummy = c;
+//     //     if(c==palabra[0])
+//     //     {
+//     //         dummy=oracion[c+palabraLength];
+//     //         std::cout << dummy << std::endl;
+//     //         if(dummy == palabra[palabraLength-1])
+//     //             count++;            
+//     //     }
+//     //     cout<<c<<"\n";
+//     // }
+//     std::cout << "La palabra '"<< palabra<<"' se repite "<< count<<" veces"<< std::endl;
+//     char dummy2;
+//     for(char const &c:oracion)
+//     {
+//         j++;
+//         if(c == palabra[0])
+//         {
+//             for (int i = 0; i < palabraLength; i++)
+//             {
+//                 dummy=palabra[i];
+//                 dummy2=oracion[j+i];
+//                 cout<<dummy2<<"\n";
+//             }
+//             if(dummy==dummy2)
+//                 count++;
+//         }
+//     }
+// }
+
+// 4-------------------------------------------------------------------------
+
+
+
+// 5-------------------------------------------------------------------------
+
+class Animal
+{
+    
+};
+
+// main----------------------------------------------------------------------
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
